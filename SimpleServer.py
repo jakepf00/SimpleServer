94465282febdc1a911bea967dc71d747ce5407fb
Jacob Pfeifer <jakepf00@gmail.com>
2024-04-06 10:23:02 -0500

    Serve files

--- SimpleServer.py
+++ SimpleServer.py
@@ -2,25 +2,34 @@ import http.server
 import os
 
 class RequestHandler(http.server.BaseHTTPRequestHandler):
-    Page = '''\
+    Error_Page = '''\
 <html>
 <body>
-    <table>
-        <tr><td>Header</td><td>Value</td></tr>
-        <tr><td>Date and Time</td><td>{date_time}</td></tr>
-        <tr><td>Client host</td><td>{client_host}</td></tr>
-        <tr><td>Client port</td><td>{client_port}</td></tr>
-        <tr><td>Command</td><td>{command}</td></tr>
-        <tr><td>Path</td><td>{path}</td></tr>
-    </table>
+    <h1>Error accessing {path}</h1>
+    <p>{msg}</p>
 </body>
 </html>
 '''
 
     # Handle a GET request.
     def do_GET(self):
-        page = self.create_page()
-        self.send_page(page)
+        try:
+            # Figure out what exactly is being requested
+            full_path = os.getcwd() + self.path
+
+            # It doesn't exist...
+            if not os.path.exists(full_path):
+                raise Exception("'{0}' not found".format(self.path)) # TODO: ServerException
+            # ...it.s a file...
+            elif os.path.isfile(full_path):
+                self.handle_file(full_path)
+            # ...it's something we don't handle
+            else:
+                raise Exception("Unknown object '{0}'".format(self.path)) # TODO: ServerException
+            
+        # Handle errors
+        except Exception as msg:
+            self.handle_error(msg)
     
     def create_page(self):
         values = {
@@ -33,12 +42,25 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         page = self.Page.format(**values)
         return page
     
-    def send_page(self, page):
-        self.send_response(200)
+    def send_content(self, content, status = 200):
+        self.send_response(status)
         self.send_header("Content-Type", "text/html")
-        self.send_header("Content-Length", str(len(page)))
+        self.send_header("Content-Length", str(len(content)))
         self.end_headers()
-        self.wfile.write(page.encode("utf-8"))
+        self.wfile.write(content)
+    
+    def handle_file(self, full_path):
+        try:
+            with open(full_path, 'rb') as reader:
+                content = reader.read()
+            self.send_content(content)
+        except IOError as msg:
+            msg = "'{0}' cannot be read: {1}".format(self.path, msg)
+            self.handle_error(msg)
+    
+    def handle_error(self, msg):
+        content = self.Error_Page.format(path = self.path, msg = msg).encode("utf-8")
+        self.send_content(content, 404)
 
 #-----------------------------------------------------------
 if __name__ == '__main__':
