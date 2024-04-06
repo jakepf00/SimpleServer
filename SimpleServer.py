bf7880209ed9a7f14b1fae64213871355c8e9a49
Jacob Pfeifer <jakepf00@gmail.com>
2024-04-06 10:24:41 -0500

    Remove unneccesary code

--- SimpleServer.py
+++ SimpleServer.py
@@ -31,17 +31,6 @@ class RequestHandler(http.server.BaseHTTPRequestHandler):
         except Exception as msg:
             self.handle_error(msg)
     
-    def create_page(self):
-        values = {
-            'date_time' : self.date_time_string(),
-            'client_host' : self.client_address[0],
-            'client_port' : self.client_address[1],
-            'command' : self.command,
-            'path' : self.path
-        }
-        page = self.Page.format(**values)
-        return page
-    
     def send_content(self, content, status = 200):
         self.send_response(status)
         self.send_header("Content-Type", "text/html")
